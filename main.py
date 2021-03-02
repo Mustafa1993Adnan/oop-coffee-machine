380e1182c25538237559402c4bebb0be23daa5ef
Mustafa1993Adnan <Mustafa1993Adnan@gmail.com>
2021-03-02 23:11:01 +0300

    change

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
-coffee_maker.report()
-money_machine.report()
 menu = Menu()
 is_machine_on = True
 while is_machine_on:
